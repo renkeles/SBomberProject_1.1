faee6ea4c3e3b7b9543f7eaaffee094f46a7ead7
renkeles <jetmebass@gmail.com>
2022-02-07 15:48:40 +0500

    Добавьте файлы проекта.

---
+++ SBomberProject/SBomberProject.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <conio.h>
+
+#include "SBomber.h"
+#include "MyTools.h"
+
+using namespace std;
+using namespace MyTools;
+
+//========================================================================================================================
+
+int main(void)
+{
+    FileLoggerSingletone::getInstance().OpenLogFile("log.txt");
+
+    SBomber game;
+
+    do {
+        game.TimeStart();
+
+        if (_kbhit())
+        {
+            game.ProcessKBHit();
+        }
+
+        ScreenSingleton::getInstance().ClrScr();
+
+        game.DrawFrame();
+        game.MoveObjects();
+        game.CheckObjects();
+
+        game.TimeFinish();
+
+    } while (!game.GetExitFlag());
+
+    FileLoggerSingletone::getInstance().CloseLogFile();
+
+    return 0;
+}
